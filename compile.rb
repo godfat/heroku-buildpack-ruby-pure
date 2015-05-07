a5bd331700f50e36d8cd2d2a86758d6b0db5f54c
Lin Jen-Shin <godfat@godfat.org>
2015-05-08 01:53:26 +0800

    try out a pure ruby buildpack for heroku

---
+++ compile.rb
@@ -0,0 +1,9 @@
+
+require 'language_pack/ruby'
+
+LanguagePack::ShellHelpers.initialize_env(ARGV[2])
+pack = LanguagePack::Ruby.new(ARGV[0], ARGV[1])
+pack.topic("Compiling #{pack.name}")
+pack.log("compile") do
+  pack.compile
+end
