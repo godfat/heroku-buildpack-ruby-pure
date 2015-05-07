c1a9ec7fc4cb8b11d5a16208e712f351be662b9a
Lin Jen-Shin <godfat@godfat.org>
2015-05-08 05:07:21 +0800

    cleanup

--- compile.rb
+++ compile.rb
@@ -24,8 +24,7 @@ class LanguagePack::RubyPure < LanguagePack::Ruby
   def pipe cmd, opts
     if opts[:env] && (bundle_gemfile = ENV['BUNDLE_GEMFILE'])
       opts[:env]['BUNDLE_GEMFILE'] = bundle_gemfile
-      prefix = File.dirname(bundle_gemfile).sub(/^#{Dir.pwd}/, '')
-      puts "$HOME/#{prefix}/#{bundler_binstubs_path}:$PATH"
+      prefix = File.dirname(bundle_gemfile).sub(%r{^#{Dir.pwd}/}, '')
       set_env_override 'PATH',
                        "$HOME/#{prefix}/#{bundler_binstubs_path}:$PATH"
     end
