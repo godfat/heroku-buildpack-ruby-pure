b7384faafc53e69ba5c20d1fc4291923033590ca
Lin Jen-Shin <godfat@godfat.org>
2015-05-08 03:10:08 +0800

    ...stop, assets

--- compile.rb
+++ compile.rb
@@ -4,8 +4,13 @@ require 'language_pack/shell_helpers'
 require 'language_pack/base'
 require 'language_pack/ruby'
 
+class LanguagePack::RubyPure < LanguagePack::Ruby
+  def create_database_yml            ; end
+  def run_assets_precompile_rake_task; end
+end
+
 LanguagePack::ShellHelpers.initialize_env(ARGV[2])
-pack = LanguagePack::Ruby.new(ARGV[0], ARGV[1])
+pack = LanguagePack::RubyPure.new(ARGV[0], ARGV[1])
 pack.topic("Compiling #{pack.name}")
 pack.log("compile") do
   pack.compile
