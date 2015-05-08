4f396ad0343281f3cdf3b146f7017353c4929a0a
Lin Jen-Shin <godfat@godfat.org>
2015-05-08 16:58:27 +0800

    huh?

--- compile.rb
+++ compile.rb
@@ -36,7 +36,7 @@ class LanguagePack::RubyPure < LanguagePack::Ruby
   end
 
   def build_bundler
-    p "build_bundler #{self.class.env['BUNDLE_GEMFILE']}"
+    p "build_bundler #{self.class.env['BUNDLE_GEMFILE']} #{ENV['BUNDLE_GEMFILE']}"
     if bundle_gemfile = ENV['BUNDLE_GEMFILE']
       prefix = File.dirname(bundle_gemfile).sub(%r{^#{Dir.pwd}/}, '')
 
@@ -67,7 +67,7 @@ class LanguagePack::RubyPure < LanguagePack::Ruby
   end
 
   def pipe cmd, opts
-    p "pipe #{self.class.env['BUNDLE_GEMFILE']}"
+    p "pipe #{self.class.env['BUNDLE_GEMFILE']} #{ENV['BUNDLE_GEMFILE']}"
     if opts[:env] && (bundle_gemfile = ENV['BUNDLE_GEMFILE'])
       opts[:env]['BUNDLE_GEMFILE'] = bundle_gemfile
     end
