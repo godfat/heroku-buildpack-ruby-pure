db2b9d6435be0a4e41b545c120993038096a1f6a
Lin Jen-Shin <godfat@godfat.org>
2015-05-08 18:44:01 +0800

    heroku bug??

--- compile.rb
+++ compile.rb
@@ -92,7 +92,9 @@ LanguagePack::ShellHelpers.initialize_env(ARGV[2])
 pack = LanguagePack::RubyPure.new(ARGV[0], ARGV[1])
 pack.topic("Compiling #{pack.name}")
 pack.log("compile") do
-  puts `ls /app/tmp/cache/vendor/heroku`
-  puts `cat /app/tmp/cache/vendor/heroku/stack`
-  pack.compile
+  Dir.chdir(build_path) do
+    puts `ls /app/tmp/cache/vendor/heroku`
+    puts `cat /app/tmp/cache/vendor/heroku/stack`
+    pack.compile
+  end
 end
