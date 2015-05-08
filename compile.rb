1424bf712a7cb7a50225e6c673b2650b5b33f53a
Lin Jen-Shin <godfat@godfat.org>
2015-05-08 19:36:01 +0800

    gosh, finally working? this is so freaking silly

--- compile.rb
+++ compile.rb
@@ -17,14 +17,11 @@ end
 module Cache
   prepend Prepend
   def copy from, to
-    puts "FROM: #{prepend(from)}, TO: #{prepend(to)}"
     super(prepend(from), prepend(to))
   end
 
   def exists? key
-    r = super(prepend(key))
-    puts "EXISTS? #{key} => #{r}"
-    r
+    super(prepend(key))
   end
 end
 
@@ -43,42 +40,18 @@ module Metadata
   def write key, value, isave=true
     full_key = prepend("vendor/heroku/#{key}")
     FileUtils.mkdir_p(File.dirname(full_key))
-    puts "WRITE: #{full_key}, #{value}"
     File.write(full_key, "#{value}\n")
     save if isave
   end
 end
 
-module Bad
-  def old?
-    puts "old? #{super}"
-    super
-  end
-
-  def exists?
-    puts "exists? #{super}"
-    super
-  end
-
-  def convert_stack stack_change
-    puts "convert_stack: #{stack_change}"
-    super
-  end
-end
-
-LanguagePack::Cache.prepend Cache
+LanguagePack::Cache   .prepend Cache
 LanguagePack::Metadata.prepend Metadata
-LanguagePack::BundlerCache.prepend Bad
 
 class LanguagePack::RubyPure < LanguagePack::Ruby
   def create_database_yml            ; end
   def run_assets_precompile_rake_task; end
   def default_process_types          ; end
-
-  def new_app?
-    puts "false new_app?"
-    false
-  end
 
   def self.bundler
     @bundler ||= begin
@@ -123,12 +96,5 @@ LanguagePack::ShellHelpers.initialize_env(ARGV[2])
 pack = LanguagePack::RubyPure.new(ARGV[0], ARGV[1])
 pack.topic("Compiling #{pack.name}")
 pack.log("compile") do
-  puts "CACHE: #{ARGV[1]}"
-  puts `ls -a #{ARGV[1]}/cedar-14/app/vendor/bundle`
-  puts "HEROKU??"
-  puts `find #{ARGV[1]} -name heroku`
-  puts `ls #{ARGV[1]}/vendor/heroku`
-  puts "--"
-  puts `ls #{ARGV[1]}/app/vendor/heroku`
   pack.compile
 end
